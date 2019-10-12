2628253d2ba7fef910f80747ff8ca69d05b6df85
Anderson Ignacio da Silva <anderson@aignacio.com>
2019-10-12 13:11:39 +0200

    Added debug configuration file for riscv on fpga for VS Code

--- sw/hello_world/src/main.c
+++ sw/hello_world/src/main.c
@@ -13,7 +13,7 @@
 extern uint32_t  _start_vector;
 volatile uint32_t* const printf_buffer = (uint32_t*) PRINTF_VERILATOR;
 
-ssize_t _write(int fildes, const void *buf, size_t nbyte) {
+size_t _write(int fildes, const void *buf, size_t nbyte) {
     const uint8_t* cbuf = (const uint8_t*) buf;
     for (size_t i = 0; i < nbyte; ++i) {
         *printf_buffer = cbuf[i];
@@ -25,16 +25,16 @@ int main(void) {
     int test = 0;
     bool toggle = false;
 
-    // for (int i = 0;i<32;i++)
-    //     set_gpio_pin_direction(i,DIR_OUT);
+    for (int i = 0;i<32;i++)
+        set_gpio_pin_direction(i,DIR_OUT);
 
     while(1){
-        // for (int i = 0;i<32;i++)
-            // set_gpio_pin_value(i,toggle);
+        for (int i = 0;i<32;i++)
+            set_gpio_pin_value(i,toggle);
 
-        // toggle = !toggle;
+        toggle = !toggle;
 
-        // for (int i=0;i<1000000;i++);
-        printf("\nHello World... %d %p %x", test++, &test, test);
+        for (int i=0;i<1000000;i++);
+        // printf("\nHello World... %d %p %x", test++, &test, test);
     };
 }
